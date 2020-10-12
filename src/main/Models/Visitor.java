8aa9ccbf8be42a3eb42bef5d11f545104c1ef4ba
Joseph Saltalamacchia <jks6634@rit.edu>
2020-10-11 23:53:02 -0400

    Added Generic request model to facilitate organizing requests

--- src/main/Models/Visitor.java
+++ src/main/Models/Visitor.java
@@ -19,8 +19,8 @@ public class Visitor {
     private String address;
     private int phoneNumber;  //TODO:do we want this to be a string or an integer?
     private int ID;
-    private ArrayList<CheckedOut> booksCheckedOut;  //this structure means that the first copy of a
-    private int totalFines;
+    private ArrayList<Book> booksCheckedOut;  //this structure means that the first copy of a
+    private double totalFines;   //TODO this can probably be removed and replaced with a class that calculates the value
 
     /**
      * A constructor to create a new Visitor, using the first name, last name, address, phone number, and ID
@@ -30,7 +30,7 @@ public class Visitor {
      * @param address  The address of the visitor
      * @param phoneNumber  The phone number of the visitor
      * @param ID  The unique ID number of the visitor
-
+     */
     public Visitor(String firstName, String lastName, String address, int phoneNumber, int ID) {
         this.firstName = firstName;
         this.lastName = lastName;
@@ -38,7 +38,6 @@ public class Visitor {
         this.phoneNumber = phoneNumber;
         this.ID = ID;
     }
-    **/
 
     /**
      * A constructor to create a new Visitor, using the first name, last name, address, phone number, ID, and list of books Checked out
@@ -48,9 +47,9 @@ public class Visitor {
      * @param address  The address of the visitor
      * @param phoneNumber  The phone number of the visitor
      * @param ID  The unique ID number of the visitor
-     * @param booksCheckedOut A list of currenty checked out books by the visitor, organized by the book's ISBN
+     * @param booksCheckedOut A list of books currently checked out by the visitor, organized by the book's ISBN
      */
-    public Visitor(String firstName, String lastName, String address, int phoneNumber, int ID, ArrayList<CheckedOut> booksCheckedOut) {
+    public Visitor(String firstName, String lastName, String address, int phoneNumber, int ID, ArrayList<Book> booksCheckedOut) {
         this.firstName = firstName;
         this.lastName = lastName;
         this.address = address;
@@ -133,10 +132,6 @@ public class Visitor {
 
     //there is no "set" method for the ID because it should never change.
 
-    public int getTotalFines() {
-        return totalFines;
-    }
-
     /**
      * adds a book to the list of books the visitor currently has checked out
      * @param book the book being checked out
@@ -145,23 +140,14 @@ public class Visitor {
     public Boolean addCheckedOutBook(Book book)
     {
         if(this.booksCheckedOut.size() < 5) {
-            int ISBN = book.getISBN();
-            Date CheckoutDate = new Date();
-            CheckedOut checkout = new CheckedOut(ISBN, this.ID, CheckoutDate);
-            this.booksCheckedOut.add(checkout);
-            return true;
+            return this.booksCheckedOut.add(book);
         }
         return false;
     }
 
-    public ArrayList<CheckedOut> getCheckouts() {
-        return this.booksCheckedOut;
-    }
-
     /**
      * removes a book from the list of books the visitor currently has checked out
-     * and uses the strategy pattern to calculate a fine (context)
-     * @param checkedOut the book being checked in
+     * @param checkedOut: the book being checked in
      * @return if the book was successfully removed from the list of books checked out.
      */
     public int checkInBook(CheckedOut checkedOut) {
@@ -195,4 +181,4 @@ public class Visitor {
 
 
 
-  }
+}
